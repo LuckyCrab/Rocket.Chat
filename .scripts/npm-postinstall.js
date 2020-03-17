c3bc4b811e04c60a9adff1603c264b41caca4897
Alex Remisov <ar@opendev.com>
2020-03-17 13:35:10 +0400

    fix npm-postinstall

--- .scripts/npm-postinstall.js
+++ .scripts/npm-postinstall.js
@@ -11,4 +11,3 @@ execSync('cp node_modules/katex/dist/fonts/* public/fonts/');
 execSync('cp node_modules/pdfjs-dist/build/pdf.worker.min.js public/');
 
 execSync('cd node_modules/@rocket.chat/apps-engine && npm install && npm run compile');
-execSync('cp -r node_modules/@rocket.chat/apps-engine/src/* node_modules/@rocket.chat/apps-engine');
